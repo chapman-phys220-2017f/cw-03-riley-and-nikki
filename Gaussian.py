5cc119763e0db27dca51efcf99665697fc3b4267
Nikki Schwartz <user@a7f5c8f1ed7c>
2017-09-12 20:24:07 +0000

    Gaussian

---
+++ Gaussian.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import math
+def g(x):
+    return(1/math.sqrt(2*math.pi))*math.exp(-x**2/2)    
+
+def interval(f,a,b,dx):
+    l=[]
+    acc = |f(a),f(a+(N-1)dx),f(b)|
+    b = a+N*dx
+def integrate(i,dx):
