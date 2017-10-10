a0c77c41cb14d2201465c3494450b72bd3cb3ecd
Justin Dressel <prof.justin.dressel@gmail.com>
2017-10-09 23:47:56 -0700

    Instructor comments

--- gaussian.py
+++ gaussian.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import math
+import math     ### Put a blank line after import statements for aethetics
 def g(x):
-    return(1/math.sqrt(2*math.pi))*math.exp(-x**2/2)    
+    return(1/math.sqrt(2*math.pi))*math.exp(-x**2/2)    ### Put a blank line between function definitions. Also put a space after "return" for clarity
 def add(x):
     return (x+1)
 def interval(f,a,b,dx):
@@ -13,10 +13,10 @@ def interval(f,a,b,dx):
     return list_interval
 
 def integrate(i,dx):
-    """integrate (i:interval, dx:float) return:float"""
+    """integrate (i:interval, dx:float) return:float"""  ### Good start, but definitely make more professional docstrings
     area = 0
     for fval in i:
-        area += fval * dx
+        area += fval * dx     ### Note that this adds one too many rectangles, so over-estimates the area
     return area
     
     
